05cdf6dfbb9e56d8b16ea4f5656a8955bf099337
Hazel John <hazel.john@outlook.com>
2018-04-24 13:30:41 -0700

    Using random_state for KFold too

--- code/python/validate.py
+++ code/python/validate.py
@@ -6,7 +6,7 @@ RANDOM_STATE = 545510477
 
 def getKFoldMetrics(X, Y, k=5):
 	metrics = numpy.zeros(5)
-	validator = KFold(n_splits=k)
+	validator = KFold(n_splits=k, random_state=RANDOM_STATE)
 	for trainingIndex, testingIndex in validator.split(X):
 		predictions, probabilities = classifier.predict(X[trainingIndex], Y[trainingIndex], X[testingIndex])
 		metrics += classifier.getMetrics(Y[testingIndex], predictions, probabilities[:,1])
